b9d9e2f6c0836c86c7eff856921f8f04a9c53665
Andrea Iacono <andrea.iacono.nl@gmail.com>
2014-01-19 19:16:02 +0100

    fix on frame draw

--- proj_generic.py
+++ proj_generic.py
@@ -1,21 +1,24 @@
 import wx
-class GenericProjection():
 
+
+class GenericProjection():
     def __init__(self):
         self.projection_type = None
 
     def get_coords(self, x, centerx, y, centery, original_x, original_y, width, height):
         return (x, y)
-    
+
     #    draws a frame of the map 
     def draw_frame(self, width, height, projection_panel, dc):
         pass
         dc.SetPen(wx.Pen("black", 2))
         projection_panel.draw_parallel(-180, width, height, False, dc)
         projection_panel.draw_parallel(180, width, height, False, dc)
-        projection_panel.draw_meridian(-90, width, height, False, dc)
-        projection_panel.draw_meridian(90, width, height, False, dc)
-        
+
+        if self.projection_type == self.ProjectionType.Cylindric:
+            projection_panel.draw_meridian(-90, width, height, False, dc)
+            projection_panel.draw_meridian(90, width, height, False, dc)
+
 
     class ProjectionType:
-            (Cylindric, PseudoCylindric, Conic, Azimuthal) = range(4)
+        (Cylindric, PseudoCylindric, Conic, Azimuthal) = range(4)
